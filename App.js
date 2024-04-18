c16233662c108f54b015c84c3d798f0bad3d63f5
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-04-18 13:42:12 +0100

    New Update

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ margin: 40 }}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
