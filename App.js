b3eb9caa21be623893a2f72fa6c783e543b1051f
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-10-01 15:34:23 +0100

    App.js updated

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>I love React Native</Text>
       <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
