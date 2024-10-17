2241b84dd314d042f21c5681b61c6257eae2d128
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-10-17 17:47:15 +0100

    new changes

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { Login } from './Screens/Login';
+import { News } from './Screens/News';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Login />
     </View>
   );
