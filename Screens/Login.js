64e62ae6a560e3b4672151459db67399524c96ea
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-11-01 17:42:03 +0100

    New changes

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,20 +1,44 @@
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
 export function LogIn() {
-
+  const notes = [
+    { text: "Going to the market to get food", title: "Shopping" },
+    { text: "Meeting my friends at school", title: "Study" },
+    { text: "Telling someone about my products", title: "Marketing" },
+    { text: "Helping my boss to get new products", title: "Purchase" },
+  ]
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight, flex: 1 }}>
       <View style={styles.container}>
         <Text>Log In</Text>
-        <ScrollView>
-          <View style={styles.parent}>
-            <View style={styles.child}></View>
-            <View style={styles.child}></View>
-            <View style={styles.child}></View>
-            <View style={styles.child}></View>
-          </View>
-        </ScrollView>
+        {/* <ScrollView>
+        </ScrollView> */}
+        <View style={styles.parent}>
+          <FlatList horizontal
+            data={notes}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.child}>
+                  <Text>Title: {item.title}</Text>
+                  <Text>{item.text}</Text>
+
+                  <TouchableOpacity style={{ backgroundColor: "#216ddf", padding: 10, paddingHorizontal: 20, borderRadius: 50, display: "flex", position: "absolute", top: "50%", right: "45%" }}>
+                    <Text style={{ color: "white", textAlign: "center" }}>Edit</Text>
+                  </TouchableOpacity>
+                </View>
+              )
+            }}
+          />
+          {/* {notes.map((item, i) => {
+              return (
+                <View style={styles.child}>
+                  <Text>{i}. Title: {item.title}</Text>
+                  <Text>{item.text}</Text>
+                </View>
+              )
+            })} */}
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -36,6 +60,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#2dc3ff",
     padding: 20,
     margin: 3,
-    height: 300
+    height: 300,
   }
 })
