7bcfbcb4eb219c878de34e14025133cec88853f3
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-11-07 13:47:14 +0100

    New changes

--- App.js
+++ App.js
@@ -1,7 +1,43 @@
 import { View } from 'react-native';
 import { LogIn } from './Screens/Login';
+import { useCallback, useEffect, useState } from "react"
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { RubikWetPaint_400Regular } from "@expo-google-fonts/rubik-wet-paint"
+import { RubikMaze_400Regular } from "@expo-google-fonts/rubik-maze"
+
+
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({ RubikWetPaint_400Regular });
+        await Font.loadAsync({ RubikMaze_400Regular });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <LogIn />
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,3 +1,6 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useState } from "react";
 import { FlatList, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Avatar, Button, Card, Drawer } from "react-native-paper"
@@ -17,16 +20,16 @@ export function LogIn() {
 
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight, flex: 1 }}>
-      <View style={styles.container}>
-        <Text>Log In</Text>
+      <View style={[styles.container, { color: "red" }]}>
+        <Text style={{ padding: 20, fontSize: 30, fontFamily: "RubikMaze_400Regular" }}>Log In</Text>
         <ScrollView>
           <View style={styles.parent}>
-            <FlatList horizontal
+            <FlatList horizontal={true}
               data={notes}
               renderItem={({ item }) => {
                 return (
                   <View style={styles.child}>
-                    <Text>Title: {item.title}</Text>
+                    <Text style={{ fontSize: 20, fontFamily: "RubikWetPaint_400Regular" }}>Title: {item.title}</Text>
                     <Text>{item.text}</Text>
 
                     <TouchableOpacity style={{ backgroundColor: "#216ddf", padding: 10, paddingHorizontal: 20, borderRadius: 50, display: "flex", position: "absolute", top: "50%", right: "45%" }}>
@@ -36,14 +39,6 @@ export function LogIn() {
                 )
               }}
             />
-            {/* {notes.map((item, i) => {
-              return (
-                <View style={styles.child}>
-                <Text>{i}. Title: {item.title}</Text>
-                <Text>{item.text}</Text>
-                </View>
-                )
-                })} */}
           </View>
 
           <Button mode='contained'>Clear</Button>
@@ -65,9 +60,15 @@ export function LogIn() {
             <Card.Content>
               <Text variant="titleLarge">David went to the City of NY, to see his parents</Text>
               <Text variant="bodyMedium">Card content</Text>
+              <View style={{ alignItems: "flex-end", marginBottom: 10 }}>
+                <FontAwesomeIcon icon={faShare} size={20} color="#216ddf" />
+              </View>
             </Card.Content>
             <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
             <Card.Actions>
+              <TouchableOpacity>
+                <FontAwesomeIcon icon={faHeart} size={30} color="#216ddf" />
+              </TouchableOpacity>
               <Button>Cancel</Button>
               <Button>Ok</Button>
             </Card.Actions>
