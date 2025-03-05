ce9f5a02ef37ef7cceb7202f6b5f768e68c71f23
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-03-05 15:30:01 +0100

    Code added

---
+++ HomescreenTest.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HomescreenTest() {
+    return (
+        <View>
+            <Text>HomescreenTest</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
