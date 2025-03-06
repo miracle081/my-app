df5b7ea5c999e2f2e345f1bced6e7a51f2940dd0
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-03-06 13:52:00 +0100

    New update

--- App.js
+++ App.js
@@ -1,47 +1,41 @@
-import { View } from 'react-native';
-import { LogIn } from './Screens/Login';
-import { useCallback, useEffect, useState } from "react"
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-import { RubikWetPaint_400Regular } from "@expo-google-fonts/rubik-wet-paint"
-import { RubikMaze_400Regular } from "@expo-google-fonts/rubik-maze"
-
-
-// SplashScreen.preventAutoHideAsync();
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await Font.loadAsync({ RubikWetPaint_400Regular });
-        await Font.loadAsync({ RubikMaze_400Regular });
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
   return (
-    <View style={{ flex: 1 }}>
-      <LogIn />
-    </View>
-  );
+    <ImageBackground source={{ uri: "https://dianapps.com/blog/wp-content/uploads/2022/12/How-to-improve-the-performance-of-a-React-Native-app-2048x1152.png" }} style={{ height: "100%", width: "100%", }}>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 30, color: "white", textAlign: "center" }}>React Native</Text>
+        <Text style={styles.text}>
+          React native is a framework for building mobile applications with JavaScript and React.
+        </Text>
+        <Image source={require("./assets/rn.jpg")} style={styles.img} />
+      </View>
+    </ImageBackground>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginTop: 60,
+    // backgroundColor: '#3d3c4b',
+    height: "100%",
+  },
+  text: {
+    fontSize: 18,
+    color: "#ffffff",
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#ffffff10",
+    width: "100%",
+  },
+  img: {
+    width: "100%",
+    height: 200,
+    marginTop: 20,
+    borderRadius: 10,
+  }
+})
