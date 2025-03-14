7c9d755cfcfcb0a33a89df191bdfff48c22ff104
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-03-14 13:48:33 +0100

    New update

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,11 +1,19 @@
-import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity, ScrollView, Alert, FlatList, } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity, ScrollView, Alert, FlatList, SafeAreaView, StatusBar, } from "react-native";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faGoogle, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { ActivityIndicator, MD2Colors, Snackbar } from "react-native-paper";
 
 export function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [users, setUsers] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [visible, setVisible] = useState(false);
 
+  const onToggleSnackBar = () => setVisible(!visible);
+
+  const onDismissSnackBar = () => setVisible(false);
 
   function handleLogin() {
     // console.log("Button clicked 2");
@@ -17,6 +25,8 @@ export function Login() {
     setUsers([...users, data]);
     setUsername("");
     setPassword("");
+    setloading(!loading)
+    onToggleSnackBar()
     // Alert.alert(
     //   "Login",
     //   "Login Successful",
@@ -27,16 +37,13 @@ export function Login() {
     // );
   }
 
-  // console.log("Username: ", username);
-
 
 
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={{ flex: 1, justifyContent: "center" }}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ flex: 1, justifyContent: "center", padding: 20, }}>
         <Text style={styles.text}>Login</Text>
 
-        <Text style={styles.text}>{username}</Text>
         <TextInput
           style={styles.inp}
           placeholder="Username"
@@ -58,19 +65,35 @@ export function Login() {
         <Text style={{ textAlign: "center", marginTop: 10, fontSize: 20, color: "gray" }}>OR</Text>
         <View style={{ flexDirection: "row", gap: 10, }}>
           <TouchableOpacity style={{ flex: 1, borderColor: "#ebb382", padding: 10, borderRadius: 50, marginVertical: 10, borderWidth: 1 }}>
-            <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold", color: "#ebb382" }}>Google</Text>
+            {/* <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold", color: "#ebb382" }}>Google</Text> */}
+            <FontAwesomeIcon icon={faGoogle} size={25} color="#ebb382" style={{ alignSelf: "center" }} />
           </TouchableOpacity>
           <TouchableOpacity style={{ flex: 1, borderColor: "#ebb382", padding: 10, borderRadius: 50, marginVertical: 10, borderWidth: 1 }}>
-            <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold", color: "#ebb382" }}>X</Text>
+            {/* <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "bold", color: "#ebb382" }}>X</Text> */}
+            <FontAwesomeIcon icon={faXTwitter} size={25} color="#ebb382" style={{ alignSelf: "center" }} />
           </TouchableOpacity>
         </View>
 
-        <View style={{ flexWrap: "wrap", flexDirection: "row", flex: 1, backgroundColor: "#0000001d", marginTop: 30 }}>
+        <ActivityIndicator animating={loading} color={MD2Colors.red800} size={90} />
+
+        {/* <Snackbar
+          visible={visible}
+          onDismiss={onDismissSnackBar}
+          action={{
+            label: 'Undo',
+            onPress: () => {
+              // Do something
+            },
+          }}>
+          Hey there! I'm a Snackbar.
+        </Snackbar> */}
+
+        {/* <View style={{ flexWrap: "wrap", flexDirection: "row", flex: 1, backgroundColor: "#0000001d", marginTop: 30 }}>
           <View style={{ backgroundColor: "green", width: 110, height: 110, margin: 3, position: "absolute", right: 0, top: -20, zIndex: 1 }} />
           <View style={{ backgroundColor: "gray", width: 100, height: 100, margin: 3, }} />
           <View style={{ backgroundColor: "gray", width: 100, height: 100, margin: 3, }} />
           <View style={{ backgroundColor: "gray", width: 100, height: 100, margin: 3, }} />
-        </View>
+        </View> */}
 
 
 
@@ -87,15 +110,14 @@ export function Login() {
           );
         }}
       /> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    // marginTop: 50,
-    padding: 20,
+    marginTop: StatusBar.currentHeight,
     // justifyContent: "center",
     // flexDirection: "row",
     // alignItems: "center"
