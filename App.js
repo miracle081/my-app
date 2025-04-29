476dca872f27bb48c1a8f2c685bde381bac87498
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-04-29 15:07:21 +0100

    New updates

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import { View, Text, Image } from 'react-native';
-import { Layout } from './Screens/Layout';
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ marginTop: 60 }}>
-      <Layout />
-      {/* <Text style={{ fontSize: 30, color: "blue", textAlign: "center" }}>App - React Native.</Text>
-      <Image source={require("./assets/rn.jpg")}
-        style={{ width: "100%", height: 230, marginVertical: 20 }}
-      />
-      <Text>React Native is fun</Text> */}
+    <View style={styles.container}>
+      <Text style={{ color: "green", fontSize: 25, textAlign: "center" }} >Introduction to React Native</Text>
+      <Text>App</Text>
+
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    // backgroundColor: "gray",
+    backgroundColor: "#ececec",
+    padding: 20,
+  }
+})
